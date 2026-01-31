79773f15a56e58948bf9c0717931c143dbb7cf87
kroniak <me@kroniak.net>
2026-01-31 20:30:38 +0300

    Add shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,60 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+  import { ModeWatcher } from "mode-watcher";
+  import ModeSwitcher from "../lib/components/app/mode-switcher.svelte";
+  import AppSidebar from "$lib/components/app/app-sidebar.svelte";
+  import { Separator } from "$lib/components/ui/separator";
+  import { SidebarInset, SidebarProvider, SidebarTrigger } from "$lib/components/ui/sidebar";
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+  } from "$lib/components/ui/breadcrumb";
+
+  let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+  <link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+
+<SidebarProvider>
+  <AppSidebar />
+  <SidebarInset>
+    <header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+      <SidebarTrigger class="-ms-1" />
+      <Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem class="hidden md:block">
+            <BreadcrumbLink href="##">Building Your Application</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator class="hidden md:block" />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div class="ml-auto">
+        <ModeSwitcher />
+      </div>
+    </header>
+    <div class="flex flex-1 flex-col gap-4 p-4">
+      <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+        <div class="aspect-video rounded-xl bg-muted/50"></div>
+      </div>
+      <div class="min-h-screen flex-1 rounded-xl py-2 md:min-h-min">
+        {@render children()}
+      </div>
+    </div>
+  </SidebarInset>
+</SidebarProvider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+  import { Button } from "../lib/components/ui/button/index.ts";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>
+  <Button variant="outline" size="sm">Click me</Button>
+</p>
