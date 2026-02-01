8c59ecb6609a2f40a87fd87cec8911f203cffb32
kroniak <me@kroniak.net>
2026-02-01 17:25:47 +0300

    Update layout and styles: integrate `Geist Mono` font, arcade-themed, mono-themed layout styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,9 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@100..900&display=swap" rel="stylesheet">
 </svelte:head>
 
 <ModeWatcher />
